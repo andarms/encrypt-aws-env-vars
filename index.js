4f74f66c65245b580beb1fc16579482ffa6fc0dc
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-05 17:32:19 -0500

    add wait to promise

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   const secretAccessKey = core.getInput("aws-secret-access-key");
 
   const configFileLocation = path.join(process.env.GITHUB_WORKSPACE, inputFile);
-  const encryptedFile = encryptFile({
+  const encryptedFile = await encryptFile({
     region,
     cmk,
     configFileLocation,
