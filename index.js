fe0bb62897fd4cb79939b8257e5400b203d1cdd1
Adrian Manjarres <amanja4@bancodebogota.com.co>
2021-05-05 17:33:31 -0500

    add async await

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const github = require("@actions/github");
 const path = require("path");
 const { encryptFile } = require("./src/encrypt");
 
-try {
-  const region = core.getInput("aws-region");
-  const cmk = core.getInput("aws-cmk-arn");
-  const inputFile = core.getInput("config-file-location");
-  const accessKeyId = core.getInput("aws-access-key-id");
-  const secretAccessKey = core.getInput("aws-secret-access-key");
+async function main() {
+  try {
+    const region = core.getInput("aws-region");
+    const cmk = core.getInput("aws-cmk-arn");
+    const inputFile = core.getInput("config-file-location");
+    const accessKeyId = core.getInput("aws-access-key-id");
+    const secretAccessKey = core.getInput("aws-secret-access-key");
 
-  const configFileLocation = path.join(process.env.GITHUB_WORKSPACE, inputFile);
-  const encryptedFile = await encryptFile({
-    region,
-    cmk,
-    configFileLocation,
-    accessKeyId,
-    secretAccessKey,
-  });
-  core.setOutput("encrypted-file-location", encryptedFile);
+    const configFileLocation = path.join(
+      process.env.GITHUB_WORKSPACE,
+      inputFile
+    );
+    const encryptedFile = await encryptFile({
+      region,
+      cmk,
+      configFileLocation,
+      accessKeyId,
+      secretAccessKey,
+    });
+    core.setOutput("encrypted-file-location", encryptedFile);
 
-  console.log(encryptedFile);
-} catch (error) {
-  core.setFailed(error.message);
+    console.log(encryptedFile);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
